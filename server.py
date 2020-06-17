21b983f1686ee0ae33305a61a742dc5c68929a79
harish1908 <64888005+harish1908@users.noreply.github.com>
2020-06-17 22:56:51 +0530

    final

--- server.py
+++ server.py
@@ -2,24 +2,57 @@ import mysql.connector
 import datetime
 import requests
 
-mydb = mysql.connector.connect(host='localhost',user='root',password='pass@123',database="test")
-cursor = mydb.cursor()
-
-cursor.execute("select datetime from employee")
-
-res = cursor.fetchall()
-for data in res :
-    print(datetime.time())
-
-
-# data to be sent to an url
-data = "hello"
-
 # url on which data to be sent
 url ="http://pastebin.com/api/api_post.php"
 
+mydb = mysql.connector.connect(host='localhost',user='root',password='pass@123',database="walkover")
+cursor = mydb.cursor()
 
-r=requests.post(url=url, data = data)
+cursor.execute("select userid from ms_user_paid_signup")
+
+#array to store paid userid
+paid_id = cursor.fetchall()
+
+cursor.execute("select user_userid from ms_user")
+
+# array of user_userid 
+userid = cursor.fetchall()
+
+
+
+# list of queries
+
+userdate = "select user_date from ms_user where user_userid = %s "
+userdata ="select user_uname,user_email, user_mobno,user_userid from ms_user where user_userid = %s"
+
+for i in range(len(userid)) :
+    ser_ele = -1
+    for j in range(len(paid_id)) :
+        if paid_id[j] == userid[i] :
+            ser_ele = j
+            break
+
+    if ser_ele < 0 :
+        cursor.execute(userdate,userid[i])
+        udate = cursor.fetchone()
+        diff = (datetime.datetime.now()-udate[0]).total_seconds()
+        if diff > 1800 :
+            cursor.execute(userdata,userid[i])
+            result = cursor.fetchone()
+            data = {
+                'uname' : result[0],
+                'mail' : result[1],
+                'mob' : result[2],
+                'uid' : result[3]
+            }
+            r=requests.post(url=url, data = data)
+            print(r)
+
+        else :
+            print("time less than 30 minutes")
+
+    else :
+        print("user has paid signup for uid  %s",userid[i])                
 
 
 
