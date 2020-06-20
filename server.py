41e37725f822c35f403ee53f0103f884f05190b2
harish1908 <64888005+harish1908@users.noreply.github.com>
2020-06-20 19:38:18 +0530

    optimized

--- server.py
+++ server.py
@@ -8,25 +8,25 @@ url ="https://sokt.io/zuMeLkivxyDsMyBWJHsW/personal-harish1908"
 mydb = mysql.connector.connect(host='localhost',user='root',password='pass@123',database="walkover")
 cursor = mydb.cursor()
 
-cursor.execute("SELECT user_userid FROM ms_user WHERE TIMESTAMPDIFF(MINUTE, user_date, now()) BETWEEN 30 AND 60  and user_userid NOT IN ( SELECT userid FROM ms_user_paid_signup)")
+cursor.execute("SELECT user_uname,user_email, user_mobno,user_userid FROM ms_user WHERE TIMESTAMPDIFF(MINUTE, user_date, now()) BETWEEN 30 AND 1600000000000  and user_userid NOT IN ( SELECT userid FROM ms_user_paid_signup)")
 
 #array of unpaid_signup and tim between 30 and 60
-userid = cursor.fetchall()
+userdata = cursor.fetchall()
 
-#querry
-userdata ="select user_uname,user_email, user_mobno,user_userid from ms_user where user_userid = %s"
+ 
 
-for i in range(len(userid)) :
-    cursor.execute(userdata,userid[i])
-    result = cursor.fetchone()
+for i in range(len(userdata)) :
     data = {
-                'uname' : result[0],
-                'mail' : result[1],
-                'mob' : result[2],
-                'uid' : result[3]
+                'uname' : userdata[i][0],
+                'mail' : userdata[i][1],
+                'mob' : userdata[i][2],
+                'uid' : userdata[i][3]
             }
     r=requests.post(url=url, data = data)
-    print(r)        
+    print(r)       
+            
+
+     
 
 
 
